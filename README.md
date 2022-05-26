be25a595d8428cf692aaa4e81c4d3160ee8b4603
Yash Srivastava <ysrivastava82@gmail.com>
2022-05-26 11:45:00 +0530

    Second_Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Alzemist
-A Deep Learning Model to see which classifies Brain MRI on the basis of the dementia (AD). 
+A Deep Learning Model to see which classifies Brain MRI on the basis of the dementia (AD). The images are classified as follows - Mildly Demented, Moderate Demented, Non Demented, Very Mild Demented.
+
+## Overview : Alzemist
+
+
+## Why Alzeimers ?
+It is one of my biggest fears. I want to tackle it with tools that I am decent at.
+
+## The Making Of : Alzeimist
+Alzemist is something that has been on top of my mind for a long time. Neurodegenrative disorders, or in general the ability of lose control of your mind is one of my biggest fears. While I can't help cure it, I can do something that I am half good at and Alzemist is just that.  
+
--- data_processing.py
+++ data_processing.py
@@ -4,23 +4,10 @@
 from glob import glob
 import tensorflow as tf
 CLASSES = 5
+IMG_SIZE = (50, 50)
 
 """ Running this is actually really beneficial """
 
 #  Check this:
-train_ds = tf.keras.utils.image_dataset_from_directory("Alz_Dataset/train", colormode='grayscale' seed=312, label_mode='int')
-print(train_ds.class_names)
-
-
-test_ds = tf.keras.utils.image_dataset_from_directory("Alz_Dataset/test", colormode='grayscale' seed=172, label_mode='int')
-print(test_ds.class_names)
-
-Debug: print(
-    "[INFO]: ......... Preparing Train Dataset and saving as NumPy Array .......")
-
-print("[INFO]: ......... Saved Train Dataset ........... ")
-
-
-print("[INFO]: ......... Saved Test Dataset ........... ")
-
-print("[INFO]: ......... Done with Preparing the Dataset - Now run 'SpiceyDicey.py' ........... ")
+train_ds = tf.keras.utils.image_dataset_from_directory( directory = "Alz_Dataset/train", colormode='grayscale' seed=312, label_mode='int', image_size=IMG_SIZE)
+test_ds = tf.keras.utils.image_dataset_from_directory( directory = "Alz_Dataset/test", colormode='grayscale' seed=172, label_mode='int', image_size=IMG_SIZE)
